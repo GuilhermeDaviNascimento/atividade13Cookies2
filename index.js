a53226e0af19b32fa9effd6da0a8c400aba1cfc6
Glitch (adhesive-lizard-message) <none>
2024-02-26 04:03:31 +0000

    💳🏝 Checkpoint ./index.js:55573329/8743 ./views/pages/index.ejs:55573329/374

--- index.js
+++ index.js
@@ -1,122 +1,117 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const { createRequire } = require('module');
-
-const require = createRequire(import.meta.url);
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const path = require("path");
 
 const app = express();
 let upload = multer();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => res.render('pages/index'));
-app.get('/login', (req, res) => res.render('pages/login'));
-app.get('/register', (req, res) => res.render('pages/register'));
+app.get("/", (req, res) => res.render("pages/index"));
+app.get("/login", (req, res) => res.render("pages/login"));
+app.get("/register", (req, res) => res.render("pages/register"));
 
-const __filename = require.resolve(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 let users = [];
 
 const addUser = (email, user, pass) => {
-    const newConta = {
-        email: email,
-        user: user,
-        pass: pass
-    };
-    users.push(newConta);
+  const newConta = {
+    email: email,
+    user: user,
+    pass: pass,
+  };
+  users.push(newConta);
 };
 
-
-app.post('/sucess', (req, res) => {
-    if (users.find(login => login.user === req.body.user && login.pass === req.body.pass)) {
-        res.render('pages/sucess')
-        res.status(200)
-    } else {
-        res.render('pages/invalid')
-        res.status(401)
-    }
-}
-);
+app.post("/sucess", (req, res) => {
+  if (
+    users.find(
+      (login) => login.user === req.body.user && login.pass === req.body.pass
+    )
+  ) {
+    res.render("pages/sucess");
+    res.status(200);
+  } else {
+    res.render("pages/invalid");
+    res.status(401);
+  }
+});
 const isvalidpass = (pass, cpass) => {
-    if (pass !== cpass) {
-        return false
-    } else {
-        return true
-    }
+  if (pass !== cpass) {
+    return false;
+  } else {
+    return true;
+  }
 };
 
 const isValidEmail = (email) => {
-    if (users.some(login => login.email === email)) {
-        return false
-    } else {
-        return true
-    }
+  if (users.some((login) => login.email === email)) {
+    return false;
+  } else {
+    return true;
+  }
 };
 
 const isValidUser = (name) => {
-    if (users.some(login => login.user === name)) {
-        return false
-    } else {
-        return true
-    }
+  if (users.some((login) => login.user === name)) {
+    return false;
+  } else {
+    return true;
+  }
 };
 function deleteUserByUsername(username) {
-    const index = users.findIndex(user => user.user === username);
-    
-    if (index !== -1) {
-        users.splice(index, 1);
-        console.log(`Usuário ${username} deletado com sucesso.`);
-    } else {
-        console.log(`Usuário ${username} não encontrado.`);
-    }
-}
-app.post('/createaccount', (req, res) => {
-    let mensagem;
-    if (isValidEmail(req.body.email) === false) {
-        console.log(req.body.email)
-        mensagem = "Email já cadastrado";
-    } else if (isValidUser(req.body.user) === false) {
-        mensagem = "Usuário existente";
-    } else if (isvalidpass(req.body.pass, req.body.cpass) === false) {
-        mensagem = "Senha diferente"
-    } else {
-        addUser(req.body.email, req.body.user, req.body.pass)
-        mensagem = "Sucesso"
-    }
-    res.status(200).json({ mensagem: mensagem });
-}
-)
+  const index = users.findIndex((user) => user.user === username);
 
-app.get('/adminpage', (req, res) => {
-    res.render('pages/adminpage');
+  if (index !== -1) {
+    users.splice(index, 1);
+    console.log(`Usuário ${username} deletado com sucesso.`);
+  } else {
+    console.log(`Usuário ${username} não encontrado.`);
+  }
+}
+app.post("/createaccount", (req, res) => {
+  let mensagem;
+  if (isValidEmail(req.body.email) === false) {
+    console.log(req.body.email);
+    mensagem = "Email já cadastrado";
+  } else if (isValidUser(req.body.user) === false) {
+    mensagem = "Usuário existente";
+  } else if (isvalidpass(req.body.pass, req.body.cpass) === false) {
+    mensagem = "Senha diferente";
+  } else {
+    addUser(req.body.email, req.body.user, req.body.pass);
+    mensagem = "Sucesso";
+  }
+  res.status(200).json({ mensagem: mensagem });
 });
 
-app.post('/adminpage/data', (req, res) => {
-    let usuarios = '';
-    users.forEach(login => {
-        let label = `<div id="template"><div><label for="">Email:</label><label for="">${login.email}</label></div>
+app.get("/adminpage", (req, res) => {
+  res.render("pages/adminpage");
+});
+
+app.post("/adminpage/data", (req, res) => {
+  let usuarios = "";
+  users.forEach((login) => {
+    let label = `<div id="template"><div><label for="">Email:</label><label for="">${login.email}</label></div>
         <div><label for="">Username:</label><label for="">${login.user}</label></div>
         <button id="${login.user}" onclick="requesdelete('${login.user}')" type="button" class="btn btn-danger">Deletar</button>
         </div>`;
-        usuarios += label;
-    });
-    res.status(200).json({ mensagem: usuarios });
+    usuarios += label;
+  });
+  res.status(200).json({ mensagem: usuarios });
 });
 
-app.post('/adminpage/delete', (req, res) => {
-    try {
-        deleteUserByUsername(req.body.username)
-        res.json({ message: "User deleted successfully" });
-    } catch (error) {
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+app.post("/adminpage/delete", (req, res) => {
+  try {
+    deleteUserByUsername(req.body.username);
+    res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-app.listen(3000, () => console.log('Server ready'));
+app.listen(3000, () => console.log("Server ready"));
